2f6ba7592d35083089f0e2194b494dd3b2ed1e82
nuzulkaromah <nuzulkaromah@gmail.com>
2025-02-11 22:10:30 +0700

    Menambah template freedash dan migrations

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,5 +8,11 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests, ValidatesRequests; 
+
+    public function index() {
+        return view('pelanggan.layout.index', [
+            'title' => 'Home',
+        ]);
+    }    
 }
---
+++ database/migrations/2025_02_11_045510_create_transaksi_detail_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksi_detail', function (Blueprint $table) {
+            $table->bigIncrements('id_transaksi_detail'); 
+            $table->integer('transaksi_id')->nullable;
+            $table->integer('barang_id')->nullable;
+            $table->integer('harga');
+            $table->integer('qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksi_detail');
+    }
+};
